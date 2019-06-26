3628c674e8c2de8943089c47e02f18337da305b3
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-26 17:27:03 +0100

    MVP complete

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <ul>
+    <list-item v-for="(country, index) in countries" v-bind:country="country" v-bind:key="index"></list-item>
+  </ul>
+</template>
+
+<script>
+  import ListItem from "./ListItem.vue";
+
+  export default {
+    name: 'countries-list',
+    props: ['countries'],
+    components: {
+      "list-item": ListItem
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+</style>
